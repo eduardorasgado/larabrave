aa1032f9030aa739543a0a39b3fe862234c7d4b0
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-03-27 06:37:34 -0600

    Se agrego messasges a factories para hacer testing

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,3 +21,28 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+/*
+En Model Factories se crean modelos de usuarios o lo que necesite nuestra aplicacion de forma fake, con el fin de hacer testing automatico, o testing sin que nosotros metamos las manos en los forms, creacion de mensajes etc que necesite nuestra web app
+*/
+$factory->define(App\Message::class, function(Faker $faker){
+	return [
+		//words(5,true) true devuelve texto, con false
+		//se devuelve un array, asi mismo hay 
+		//paragraph, realText
+		//
+		'content' => $faker->realText(),
+		'image' => $faker->imageUrl(600,338)
+
+	];
+});
+
+/*
+Para activar las factories lo hacemos con Tinker, revisando en composer.json si tenemos la dependencia externa, en caso contrario, debemos integrarla
+
+tinker usualmente viene sin ser integrada directamente al
+proyecto para darnos eleccion si meterla o no en desarrollo o produccion
+
+Por otra parte una vez confirmado recurrimos a app/cinfig/app.php y vamos a providers a comprobar como integrar eso con "use" 
+
+*/
