8f1d5fb0196bb59c4c9eef4c3bc7285ae92c38af
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-04-05 06:24:45 -0500

    B: ---. F:Se modifica la estetica del formulario de subida de mensaje de la view welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,24 +11,35 @@
    <div class="row md-4">
         <!--multipart/form-data es importante para subir los archivos al server-->
        <form class="" action="{{ url('/messages/create') }}" method="POST" enctype="multipart/form-data">
-        <div class="form-group">
+        <div class="row form-group">
           
           {{-- funcion que provee laravel para generar un token --}}
           {{-- Sin ello, el form no es reconocido por laravel --}}
           {{ csrf_field() }}
 
-          <!--is-invalid es una clase de bootstrap-->
-          <input class="form-control @if($errors->has('message')) is-invalid @endif" type="text" name="message" placeholder="Qué estás pensando?" maxlength="160" size="80">  
-
-          @if($errors->has('message'))
+          <div class="col-sm-7">
+              <!--is-invalid es una clase de bootstrap-->
+            <input class="form-control @if($errors->has('message')) is-invalid @endif" type="text" name="message" placeholder="Qué estás pensando?" maxlength="160" size="80">
+            @if($errors->has('message'))
             {{-- NOs dará todos los errores relacionados al message --}}
             @foreach($errors->get('message') as $error)
               <!-- invalid-feedback tambien es de bootstrap-->
               <div class="invalid-feedback">{{ $error }}</div>
             @endforeach
-          @endif
-          {{--Input del file--}}
-            <input class="form-control-file" type="file" name="image">          
+            @endif
+          </div>  
+
+         <div class="col-sm-2">
+            <div class="custom-file">
+              {{--Input del file--}}
+              <input class="custom-file-input bg-success" id="customFile" type="file" name="image">
+              <label class="custom-file-label" for="customFile">Imagen</label>          
+            </div>
+         </div>
+         <div class="col-sm-1">
+           <button type="submit" class="btn btn-primary">Publicar</button>
+         </div>
+
         </div>
      </form>
    </div>
